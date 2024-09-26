9bc8658ab2c310496f1fbf4e85b44dc9601a2918
Harry Ramsey <harry.ramsey@arm.com>
2024-09-26 13:57:26 +0100

    Replace sprintf with snprintf
    This commit replaces sprintf which is unsafe as it can result in an
    overflow with snprintf. On MacOS this will generate a deprecated
    declaration warning using the latest clang.
    
    As these buffers are static array declarations, sizeof can be used to
    calculate the size of the buffer, however if replaced with malloc sizeof
    will no longer be correct.
    
    Signed-off-by: Harry Ramsey <harry.ramsey@arm.com>

--- programs/ssl/ssl_server.c
+++ programs/ssl/ssl_server.c
@@ -296,7 +296,7 @@ reset:
     mbedtls_printf("  > Write to client:");
     fflush(stdout);
 
-    len = sprintf((char *) buf, HTTP_RESPONSE,
+    len = snprintf((char *) buf, sizeof(buf), HTTP_RESPONSE,
                   mbedtls_ssl_get_ciphersuite(&ssl));
 
     while ((ret = mbedtls_ssl_write(&ssl, buf, len)) <= 0) {
